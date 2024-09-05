35fdef0001c78f87878dd00b8a0fdf002e4db13c
Prasun2004 <166635147+Prasun2004@users.noreply.github.com>
2024-09-05 20:39:56 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userScheme = new Schema(
+    {
+        name: { type: String, required: true },
+        username: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        token: { type: String }
+    }
+)
+
+const User = mongoose.model("User", userScheme);
+
+module.exports =User ;
